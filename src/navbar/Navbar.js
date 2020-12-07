f36beb20efde0a7119547b75cce2333ae3005a06
deZPro <dezignProjects@outlook.com>
2020-12-07 22:39:57 +0100

    Adding the first cotocrypto.pl logo concept to the Navbar.js component.

--- src/navbar/Navbar.js
+++ src/navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './Navbar.css';
 import Navcoins from './navcoins/Navcoins';
+import BookLogo from '../images/booklogo.png'
 
 function Navbar() {
     return (
         <div className="navbar">
-            <h1>LOGO</h1>
+            <div className="logo-navbar">
+                <img className="book-logo" src={BookLogo} />
+            <span className="logo-grey">cotocrypto</span><span className="logo-orange">.pl</span>
+            </div>
             <Navcoins />
         </div>
     )
